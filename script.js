63aab228ba98dbe5e8387717d0bab7b4e7772601
Vitaly-V <gtopbox@gmail.com>
2017-05-17 22:38:10 +0300

    Variable Mutation and Type Coercion

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// lecture: variables
+// Lecture: variables
 /*
 var name = 'Vitalii';
 console.log(name);
@@ -13,3 +13,30 @@ console.log(age);
 var fullAge = true;
 console.log(fullAge);
 */
+
+// Lectere: variables 2
+var name = 'Vitalii';
+var age = 32;
+
+//console.log(name + age);
+//console.log(age + age);
+
+var job, isMarried;
+
+//console.log(job);
+
+job = 'programmer';
+isMarried = true;
+
+console.log(name + ' is '  + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty two';
+job = 'director';
+
+console.log(name + ' is '  + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+
+var lastName = prompt('What is the last name?');
+console.log(lastName);
+
+alert(name + ' is '  + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
