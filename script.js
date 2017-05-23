b167f9bd6420ef015d256ad0d24c2b42816e790f
Vitaly-V <gtopbox@gmail.com>
2017-05-23 09:49:25 +0300

    Objects

--- script.js
+++ script.js
@@ -227,3 +227,33 @@ if (john.indexOf('teacher')  === -1) {
     console.log("John is not a teacher");
 }
 */
+
+// Lecture: Objects
+
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false
+}
+
+console.log(john.lastName);
+console.log(john['lastName']);
+
+var xyz = 'job';
+console.log(john[xyz]);
+
+john.lastName = 'Miller';
+john.job = 'programmer';
+
+console.log(john);
+
+var jane = new Object();
+jane.name = 'Jane';
+jane.lastName = 'Smith';
+jane['yearOfBirth'] = 1969;
+jane['job'] = 'retired';
+jane['isMarried'] = true;
+
+console.log(jane);
