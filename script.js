f19ee152eae5f01691c1ad13b30dc9ef9b6c45e5
Vitaly-V <gtopbox@gmail.com>
2017-05-23 09:31:31 +0300

    Arrays

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ yearsUntilRetirement('Mary', 1948);
 */
 
 // Lecture: Statements and expressions
-
+/*
 function someFun(par) {
     //code
 }
@@ -201,3 +201,29 @@ var x = 3;
 if (x === 5) {
     //do something
 }
+*/
+
+// Lecture: Arrays
+/*
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[1]);
+names[1] = 'Ben';
+console.log(names);
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+
+console.log(john);
+
+//alert(john.indexOf('Smith'));
+
+if (john.indexOf('teacher')  === -1) {
+    console.log("John is not a teacher");
+}
+*/
